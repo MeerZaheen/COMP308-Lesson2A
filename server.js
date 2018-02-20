4e5f513eac14a8dd5b46aa0f6f1a2091c5e7ad4b
MeerZaheen <MeerZ@Meers-MacBook-Pro.local>
2018-02-19 19:34:52 -0500

    edit

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-//our connect server 
+//our connect server
+let connect = require('connect');
+ let http = require('http');
+
+ let app = connect();
+ const port = 3000;
+
+ app.listen(port);
+ console.log("node sServer listening at port: " + port);
